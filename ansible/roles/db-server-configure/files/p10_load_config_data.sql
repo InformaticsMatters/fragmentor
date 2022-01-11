3a8dd42dec740665b654ff03d73d87f6cb7303ac
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2022-01-11 16:44:32 +0000

    additional xchem libs

--- ansible/roles/db-server-configure/files/p10_load_config_data.sql
+++ ansible/roles/db-server-configure/files/p10_load_config_data.sql
@@ -1,9 +1,10 @@
 /*
  * Load configuration data SQL Statements:
- * Purpose: Initialises venfor information
+ * Purpose: Initialises vendor information
  *
  * Author | Date    | Version
  * Duncan | 05/2020 | Initial Version
+ * Tim    | 01/2022 | Extra XChem libraries
  *
  */
 
@@ -14,3 +15,8 @@ insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_nod
 insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_node_label) values ('xchem_spot', NULL, 'Xchem-Spot','V_XSPOT');
 insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_node_label) values ('xchem_probe', NULL, 'Xchem-Probe','V_XPROB');
 insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_node_label) values ('sdf', NULL, 'SDF-File','V_SDF');
+insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_node_label) values ('xchem-leeds', NULL, 'Xchem-Leeds3D','V_XLEEDS');
+insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_node_label) values ('xchem-euopenscreen', NULL, 'Xchem-EUOpenScreen','V_XEUOPENSCR');
+insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_node_label) values ('xchem_eubopendsipext', NULL, 'Xchem-EUbOpenDsipExt', 'V_XEUBOPEN');
+insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_node_label) values ('xchem_cyselectrophile', NULL, 'Xchem-CysElectrophile','V_XCYSELEC');
+insert into vendor_name (vendor_name, currency, supplier_node_name, supplier_node_label) values ('xchem_covhet', NULL, 'Xchem-CovHet','V_XCOVHET');
