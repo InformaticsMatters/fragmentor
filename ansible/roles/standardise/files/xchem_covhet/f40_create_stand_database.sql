859ab8f841dbc736e0740775d1866595db33f7cb
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2022-01-12 10:40:49 +0000

    added covhet library

---
+++ ansible/roles/standardise/files/xchem_covhet/f40_create_stand_database.sql
@@ -0,0 +1,14 @@
+-- Create Fragmentation Database SQL Statements for Libary xchem_spot
+-- Purpose: Creates fragmentation company specific tables. This is the same as the xchem_dsip library.
+DROP TABLE IF EXISTS i_mols_dsip;
+
+-- Create i_mols
+CREATE TABLE i_mols_dsip (
+  osmiles TEXT,
+  isosmiles TEXT,
+  nonisosmiles TEXT,
+  hac SMALLINT,
+  cmpd_id TEXT,
+  isomol_id INTEGER,
+  nonisomol_id INTEGER
+);
