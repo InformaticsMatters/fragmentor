ef6100908f46970071b63be6da10df8e325bbf2e
Alan Christie <alan.christie@matildapeak.com>
2024-09-18 18:59:45 +0200

    fix: Fixes for latest test runs (fix for kubectl and non-AWS S3 buckets)

--- docker-player-entrypoint.sh
+++ docker-player-entrypoint.sh
@@ -54,8 +54,8 @@ echo "+> KUBECONFIG is ${KUBECONFIG}"
 echo "+> Copying ${KUBECONFIG_FILE} to ${KUBECONFIG}..."
 cp "${KUBECONFIG_FILE}" "${KUBECONFIG}"
 
-echo "+> kubectl version..."
-kubectl version
+echo "+> kubectl version (client)..."
+kubectl version --client
 
 # Now set default kubernetes namespace (i.e. the fragmentor)
 echo "+> kubectl config set-context..."
