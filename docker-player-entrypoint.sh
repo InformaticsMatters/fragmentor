645d04bb0581518bd9006f66a686acf6a64239aa
Alan Christie <alan.christie@matildapeak.com>
2024-12-02 10:33:57 +0000

    fix: Fix typo in --skip-tags

--- docker-player-entrypoint.sh
+++ docker-player-entrypoint.sh
@@ -81,7 +81,7 @@ echo "+> Playing ${PLAYBOOK}..."
 echo "+> FRAGMENTOR_PLAY_EXTRA_ARGS='${FRAGMENTOR_PLAY_EXTRA_ARGS}'"
 # Has the 'skip tags' environment variable been?
 if [ -n "${FRAGMENTOR_PLAY_SKIP_TAGS}" ]; then
-  FRAGMENTOR_PLAY_SKIP_TAGS="--skiptags \"${FRAGMENTOR_PLAY_SKIP_TAGS}\""
+  FRAGMENTOR_PLAY_SKIP_TAGS="--skip-tags ${FRAGMENTOR_PLAY_SKIP_TAGS}"
 fi
 
 pushd ansible || exit 1
